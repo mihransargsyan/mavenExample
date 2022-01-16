56cbd0191adaffe989158e004f2d3c8d91057d5f
mihransargsyan <misargsyan@gmail.com>
2022-01-16 15:22:36 +0400

    Add

--- src/main/java/StudentTest.java
+++ src/main/java/StudentTest.java
@@ -5,6 +5,9 @@ public class StudentTest {
     public static void main(String[] args) {
         Student  student = new Student();
         student.setName("poxos");
+        student.setSurname("poxosyan");
+        student.setAge(30);
+        student.setPhone("099854822");
         System.out.println(student);
     }
 }
