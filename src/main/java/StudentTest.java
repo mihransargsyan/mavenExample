904fb6c357aa71c44a6c0c15222a73ee55e92464
mihransargsyan <misargsyan@gmail.com>
2022-01-16 15:16:11 +0400

    Add

---
+++ src/main/java/StudentTest.java
@@ -0,0 +1,10 @@
+import model.Student;
+
+public class StudentTest {
+
+    public static void main(String[] args) {
+        Student  student = new Student();
+        student.setName("poxos");
+        System.out.println(student);
+    }
+}
